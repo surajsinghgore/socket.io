5aa64c0fad2097fefcfabc518cf1cca87ed71cef
surajsinghgore <surajthakurrs45@gmail.com>
2023-10-04 09:54:00 +0530

    alert to all,new ,only new user

--- server/server.js
+++ server/server.js
@@ -22,12 +22,24 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 
 
+//! broadcasting io.emit()
+// this will emit the event to all connected sockets
+// every one
+// only to new joiner not to all connected
+io.emit('some event', {
+  someProperty: 'some value',
+  otherProperty: 'other value'
+}); 
 
-  // listing message event from client side
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-  });
+// this will give msg to new joiner not every one
+io.on('connection', (socket) => {
+  socket.broadcast.emit('hi');
+});
 
+// every one all new + all joiner
+socket.on('chat message', (msg) => {
+  io.emit('chat message', msg);
+});
 // if connection disconnect
 socket.on('disconnect', () => {
   console.log('user disconnected');
